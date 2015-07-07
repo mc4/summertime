388d8ad0ce844c97c7f65da00aebfd9d2e88b56a
mc4 <m.conleyy@gmail.com>
2015-07-06 21:15:50 -0400

    ipinfo is really cool

---
+++ networking/ipinfo.py
@@ -0,0 +1,43 @@
+import re
+import sys
+import json
+import socket
+from urllib2 import urlopen
+
+#grabs local ip
+def get_local_ip():
+	try:
+		response = urlopen('http://checkip.dyndns.com/')
+		html = response.read()
+	except:
+		print 'error with "http://checkip.dyndns.com/"'
+		sys.exit()
+	ip = re.search('\d+.\d+.\d+.\d+', html)
+	return str(ip.group())
+
+#ip of whoever you
+ip = ''
+address = raw_input('Enter address or ip of target... or Enter for user information: ')
+
+if address:
+	ip = socket.gethostbyname(address)
+elif address == None:
+	ip = get_local_ip()
+
+#get data of ip
+ip_info = 'http://ipinfo.io/'+ip+'/json'
+data = json.load(urlopen(ip_info))
+
+#extract data
+city = data['city']
+region = data['region']
+country = data['country']
+location = data['loc']
+org = data['org']
+
+#print data
+print 'target city is: ' + city
+print 'target region is: ' + region
+print 'target country is: ' + country
+print 'target approximate location is: ' + location
+print 'target ISP(?) is: ' + org
